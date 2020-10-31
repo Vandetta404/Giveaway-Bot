bf34df15fa1a2673fc4f3562b2749828f04eea38
Hurricane <69173408+xSimplez@users.noreply.github.com>
2020-10-31 21:31:39 +0200

    Update setlang.js

--- commands/Advanced Commands/setlang.js
+++ commands/Advanced Commands/setlang.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js")
 exports.run = async (client, message, args, lang) => {
 let language = client.db.fetch(`language_${message.guild.id}`);
     if (!args[0]) return message.channel.send(lang.lang.msg);
-
-    // Arabic lang
     if (args[0] === "ar") {
         if (!message.member.hasPermission("MANAGE_GUILD")) {
 return message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription(lang.lang.perms))
@@ -14,7 +12,6 @@ return message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDe
         message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription(":flag_sa: | لغة هذا السيرفر هي العربية الآن!"))
     }
 
-    // English lang
     if (args[0] === "en") {
         if (!message.member.hasPermission("MANAGE_GUILD")) {
 return message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription(lang.lang.perms))        }
@@ -22,27 +19,5 @@ return message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDe
         if (language === "en") return message.channel.send(lang.lang.err)
         client.db.set(`language_${message.guild.id}`, "en")
         message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription(":flag_us: | The language of this server is now English!"))
-    }
-
-    // Russian lang
-    if (args[0] === "ru") {
-        if (!message.member.hasPermission("MANAGE_GUILD")) {
-return message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription(lang.lang.perms))
-        }
-
-        if (language === "ru") return message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription(lang.lang.err))
-        client.db.set(`language_${message.guild.id}`, "ru")
-       message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription(":flag_ru: | Язык изменён на Русский!"))
-    }
-
-    // Ukranian lang
-    if (args[0] === "ua") {
-        if (!message.member.hasPermission("MANAGE_GUILD")) {
-            return message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription(lang.lang.perms))
-        }
-
-        if (language === "ua") return message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription(lang.lang.err))
-        client.db.set(`language_${message.guild.id}`, "ua")
-        message.channel.send(new Discord.MessageEmbed().setColor("#00BFFF").setDescription(":flag_ua: | Мову змінено на Українську!"))
     }
 }
